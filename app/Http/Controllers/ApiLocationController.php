7a9f3dd546811b01222249b427cb1dd69b261df6
kakipi16 <kakipi533@gmail.com>
2026-03-13 17:05:55 +0900

    Basic認証追加, 401middleware追加

---
+++ app/Http/Controllers/ApiLocationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SpotPost;
+use Illuminate\Http\Request;
+
+class ApiLocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SpotPost $spotPost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SpotPost $spotPost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SpotPost $spotPost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SpotPost $spotPost)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,28 @@ Route::get('/dashboard', [googlemapsController::class, 'index'])
     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    //プロフィールの表示
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    
+    //プロフィールの編集
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/settings', [SettingsController::class, 'settings'])->name('profile.settings');
+    
+    //投稿画面の表示
+    Route::get('/googlemapsForm', [googlemapsController::class, 'googlemapsForm'])->name('googlemaps.postForm');
+    Route::post('/googlemapsForm', [googlemapsController::class, 'spotStore'])->name('SpotStore');
+    
+    //投稿詳細の表示
+    Route::get('/show/{spotPost}', [googlemapsController::class, 'show'])->name('googlemaps.show');
+    
+    //編集画面・更新処理（追加）
+    Route::get('/show/{spotPost}/edit', [googlemapsController::class, 'edit'])->name('spotPost.edit');
+    Route::patch('/show/{spotPost}', [googlemapsController::class, 'update'])->name('spotPost.update');
+    Route::delete('/show/{spotPost}', [googlemapsController::class, 'destroy'])->name('spotPost.destroy');
+    
+    //投稿一覧の表示
+    Route::get('/postList', [postListController::class, 'postList'])->name('googlemaps.postList'); 
 });
-
-Route::get('/settings', [SettingsController::class, 'settings'])->name('profile.settings');
-
-Route::get('/googlemapsForm', [googlemapsController::class, 'googlemapsForm'])->name('googlemaps.postForm');
-Route::post('/googlemapsForm', [googlemapsController::class, 'spotStore'])->name('SpotStore');
-
-Route::get('/show/{spotPost}', [googlemapsController::class, 'show'])->name('googlemaps.show');
-// 編集画面・更新処理（追加）
-Route::get('/show/{spotPost}/edit', [googlemapsController::class, 'edit'])->name('spotPost.edit');
-Route::patch('/show/{spotPost}', [googlemapsController::class, 'update'])->name('spotPost.update');
-Route::delete('/show/{spotPost}', [googlemapsController::class, 'destroy'])->name('spotPost.destroy');
-Route::get('/postList', [postListController::class, 'postList'])->name('googlemaps.postList');
 
 require __DIR__ . '/auth.php';
