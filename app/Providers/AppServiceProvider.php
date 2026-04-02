3c95564732507cf76047a3df8df984caf31a4a8b
kakipi16 <kakipi533@gmail.com>
2026-04-02 17:48:06 +0900

    デプロイまで（envエラー中）

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // config('app.env') が 'production' の時に HTTPS を強制
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,22 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/js/googlemapsAPI/main.js',
+                'resources/js/googlemapsAPI/form.js',
+                'resources/js/postfile/imgShow.js',
+                'resources/js/postfile/noFileName.js',
+                'resources/js/googlemapsAPI/spotRenderer.js'
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
