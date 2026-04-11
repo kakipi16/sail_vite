b30100bcfbf202a2d85230d9b356e00513f90ac1
kakipi16 <kakipi533@gmail.com>
2026-04-11 22:34:03 +0900

    helloが入ってた。

--- resources/js/postfile/noFileName.js
+++ resources/js/postfile/noFileName.js
@@ -2,7 +2,6 @@ const fileSelect = document.getElementById("imageButton");
 const fileElem = document.getElementById("imageUpload");
 
 fileSelect.addEventListener("click", () => {
-  console.log("hello")
   if (fileElem) {
     fileElem.click();
   }
